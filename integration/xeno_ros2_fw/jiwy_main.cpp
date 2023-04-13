8de2f2efec8eea33245044238632aa517d9d3865
Armin Purle-Kopacz <39767660+pukoarmin@users.noreply.github.com>
2023-04-13 13:50:00 +0200

    Add files via upload

--- integration/xeno_ros2_fw/jiwy_main.cpp
+++ integration/xeno_ros2_fw/jiwy_main.cpp
@@ -9,9 +9,41 @@
 #include "framework/runnableClass.h"
 #include "framework/superThread.h"
 #include "framework/icoCommClass.h"
+
+#include "Controllers/ControllerPan/ControllerPan.h"
 // #include <Your 20-sim-code-generated h-file?> Don't forget to compile the cpp file by adding it to CMakeLists.txt
 
+#define PERIOD_20sim_ns 1000000 //1ms
+
+RT_TASK rt_20sim;
+
 volatile bool exitbool = false;
+
+void exe_20sim(void *arg)
+{
+    20sim_o = new ControllerPan; 
+    int input_length = 12;
+    int output_length = 8;
+
+    int u = { 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+    int y = { 0, -1, -1, -1, -1, -1, -1, -1 }; 
+
+    IcoComm* icoComm = new IcoComm(y,u);
+    frameworkComm *controller_uPorts[] = {
+    new IDDPComm(u),
+    icoComm};
+    frameworkComm *controller_yPorts[] = {
+    new IDDPComm(y),
+    icoComm};
+
+   runnable *wrapper = new wrapper(20sim_o, controller_uPorts, controller_yPorts, input_length, output_length); 
+    wrapper->setSize(input_length, output_length);
+    wrapper->prerun();
+
+    while(1) {
+        wrapper->step();
+    }
+}
 
 void exit_handler(int s)
 {
@@ -29,10 +61,33 @@ int main()
         // you can still do proper clean-up. You are free to alter the
         // way of determining when to stop (e.g., run for a fixed time).
 
-
     // CONFIGURE, CREATE AND START THREADS HERE
     
+    /* create Xenomai task */
+    int ret;
 
+    ret = rt_task_create(&rt_20sim, "rt_20sim", 0, 50, 0);
+    if(ret < 0)
+    {
+        printf("Failed to create task: %s\n", strerror(-ret));
+        return EXIT_FAILURE;
+    }
+
+    /* make Xenomai task periodic */
+    ret = rt_task_set_periodic(&rt_20sim, TM_NOW, PERIOD_20sim_ns);
+    if(ret < 0)
+    {
+        printf("Failed to set task periodic: %s\n", strerror(-ret));
+        return EXIT_FAILURE;
+    }
+
+    /* start Xenomai task */
+    ret = rt_task_start(&rt_20sim, &exe_20sim, NULL);
+    if(ret < 0)
+    {
+        printf("Failed to start task: %s\n", strerror(-ret));
+        return EXIT_FAILURE;
+    }
 
     // WAIT FOR CNTRL-C
     timespec t = {.tv_sec=0, .tv_nsec=100000000}; // 1/10 second
@@ -45,7 +100,14 @@ int main()
     }
     printf("Ctrl-C was pressed: Stopping gracefully...\n");
 
-    //CLEANUP HERE
+    //CLEANUP HERE 
+    /* clean up and exit */
+    ret = rt_task_delete(&t_20sim);
+    if(ret < 0)
+    {
+        printf("Failed to delete task: %s\n", strerror(-ret));
+        return EXIT_FAILURE;
+    }
 
 
     return 0;
