8a9e345c82d8d747831f795a07d3500a3cf9a5f8
Purle-Kopacz <pukoarmin@gmail.com>
2023-04-14 22:32:29 +0200

    RW Convert Mockup

--- integration/xenomai-ros2-framework/jiwy_main.cpp
+++ integration/xenomai-ros2-framework/jiwy_main.cpp
@@ -21,16 +21,7 @@ volatile bool exitbool = false;
 
 void ReadConvert(const double* src, double* dst)
 {
-    static double lastKnownGoodValue = 0;S
-    /* We assume that src is a 3 element double array, which has:
-    * src[0] = temperature reading in degrees Fahrenheit (scaling)
-    * -> We want the output in degrees Celcius
-    * src[1] = some measurement with unreliable communication; once
-    * in a while a (faulty) 0 is received; this should be
-    * filtered and the previous value should be passed (filtering)
-    * src[2] = some good measurement which does not need
-    * scaling neither filtering
-    */
+    static double lastKnownGoodValue = 0;
     dst[0] = (src[0]-32.0) / 1.8; // scaling
     //if ( src[1]!=0) // filtering
     //{
@@ -42,6 +33,13 @@ void ReadConvert(const double* src, double* dst)
     //    dst[1] = lastKnownGoodValue;
     //}
     //    dst[2] = src[2]; // passing
+
+}
+
+void WriteConvert(const double* src, double* dst)
+{
+    static double lastKnownGoodValue = 0;
+    dst[0] = (src[0]-32.0) / 1.8;
 
 }
 
